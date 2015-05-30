9927ec342e6b555e83f7cc83eda8ac01c92ee937
Sameem <zsameem@gmail.com>
2015-05-30 16:52:21 +0530

    forms.py checked

--- app/forms/login.py
+++ app/forms/login.py
@@ -1,11 +1,23 @@
 from flask.ext.wtf import Form 
-from wtforms import TextField, validators, ValidationError, PasswordField, FieldField, BooleanField, RadioField
+from wtforms import TextField, validators, ValidationError, PasswordField, BooleanField, RadioField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-	email = TextField('E-mail', validators = [Required()])
+	email   = TextField('E-mail', validators = [Required()])
 	pasword = PasswordField('Password', [validators.Required('Please enter a password.')])
 
 
 class SignupForm(Form):
-	name = TextField('First name',[validators.Required('Please enter first name.')])
+	name 	    = TextField('First name',[validators.Required('Please enter first name.')])
+	last 	    = TextField('Last name',[validators.Required('Please enter last name.')])
+	department  = TextField('Department', [validators.Required('Please enter department.')])
+	year_joined = TextField('Year joined', [validators.Required('Please enter your year of joining')])
+	password    = PasswordField('Password', [validators.Required('Enter a valid password')])
+	user_type   = RadioField('Are you ', choices=[('student','student'),('faculty','faculty')])
+	enroll		= TextField('Enrollment number',[validators.Required('Please enter your enrollment no.')])
+	roll_no     = TextField('Class Roll no')
+
+
+
+
+
