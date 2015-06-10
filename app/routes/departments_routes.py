ce066c818af2485019425ff6408f2ae4f7a343ff
Sameem <zsameem@gmail.com>
2015-06-11 00:04:51 +0530

    Added models for departments and populated one field. Added manage.py

--- app/routes/departments_routes.py
+++ app/routes/departments_routes.py
@@ -10,6 +10,7 @@ def departments():
 #Departments pagez
 @nit.route('/departments/<departmentname>')
 def department_page(departmentname):
-	#only one route available becausedatabase is not populated
+	#only one route available because database is not populated
+	# http://127.0.0.1:5000/departments/ITE
 	dep = Department.query.filter_by(code = departmentname).first_or_404()
 	return render_template('department_base.html', dep = dep)
